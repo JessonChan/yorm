e00610b0e72db15fde0fb6423b0f447c6eff6d69
jessonchan <chendongpo@meituan.com>
2014-11-05 14:56:36 +0800

    add tags file

--- doc.go
+++ doc.go
@@ -1,3 +1,22 @@
 package yorm
 
 //yes-orm
+
+/**
+type Inner struct{
+	Id int64
+}
+type Table struct{
+	In Inner `yorm:"-"`
+	//this struct's expected fields will represent as column too
+	I1 Inner
+	//this situation means Inner must implent the interface to decode value
+	//and must implent the value to string
+	I2 Inner `yorm:"column(name)"`
+	//use i2 as column name
+	I2 Inner `yorm:"column()"`
+	//yorm assiagn default value to basic type ,as int string float64...
+	//also the type implents Byte2Struct
+	I3 int `yorm:"column();default()"`
+}
+**/
---
+++ tags_test.go
@@ -0,0 +1,71 @@
+package yorm
+
+import "testing"
+
+func TestParseValue_1(t *testing.T) {
+	v, set := parseBracketsValue("column()", "column")
+	if v != "" && set {
+		t.Log(v)
+		t.FailNow()
+	}
+}
+func TestParseValue_2(t *testing.T) {
+	v, set := parseBracketsValue("column(tag)", "column")
+	if v != "tag" && set {
+		t.Log(v)
+		t.FailNow()
+	}
+}
+
+func TestParseValue_3(t *testing.T) {
+	v, set := parseBracketsValue("name(jc)", "nam")
+	if v == "" && !set {
+	} else {
+		t.Fail()
+	}
+}
+
+func TestParseTag_1(t *testing.T) {
+	tag := parseTag("-")
+	if tag.skip && tag.columnName == "" && tag.defaultValue == "" {
+	} else {
+		t.Log(tag)
+		t.Fail()
+	}
+}
+func TestParseTag_2(t *testing.T) {
+	tag := parseTag("column(tag)")
+	assert(t, !tag.skip, tag.columnName == "tag", tag.defaultValue == "")
+}
+func TestParseTag_3(t *testing.T) {
+	tag := parseTag("default(1)")
+	assert(t, tag.defaultValue == "1")
+}
+func TestParseTag_4(t *testing.T) {
+	tag := parseTag("-;column(tag)")
+	assertTrue(tag.skip, t)
+	assertTrue(tag.columnName == "", t)
+}
+func TestParseTag_5(t *testing.T) {
+	tag := parseTag("column(tag1);default(1)")
+	assertTrue(!tag.skip, t)
+	assertTrue(tag.columnName == "tag1", t)
+	assertTrue(tag.defaultValue == "1", t)
+}
+func assert(t *testing.T, b ...bool) {
+	log := ""
+	for _, v := range b {
+		if !v {
+			log += "false"
+			t.Log(log)
+			t.Fail()
+		}
+		log += "true,"
+	}
+}
+
+func assertTrue(b bool, t *testing.T) {
+	if !b {
+		t.FailNow()
+	}
+}
