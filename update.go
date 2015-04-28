ca1be7caa96d9e56a985d9468370a392b05ab7e3
JessonChan <chinachendongpo@gmail.com>
2015-04-28 18:41:17 +0800

    update method

--- update.go
+++ update.go
@@ -1,1 +1,14 @@
 package yorm
+
+func Update(i interface{}, clause string, args ...interface{}) (int64, error) {
+	stmt, err := getStmt(clause)
+	if err != nil {
+		return 0, err
+	}
+	r, err := stmt.Exec(args...)
+	if err != nil {
+		return 0, err
+	}
+	id, err := r.RowsAffected()
+	return id, err
+}
