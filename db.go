a5f09d12345712eeaf93c4eab424122245f96ec8
JessonChan <chinachendongpo@gmail.com>
2015-04-30 18:03:06 +0800

    add yorm db test data set

--- db.go
+++ db.go
@@ -3,6 +3,7 @@ package yorm
 import (
 	"database/sql"
 	"reflect"
+	"sync"
 )
 
 const (
@@ -11,6 +12,7 @@ const (
 
 // main db to operate ,maybe will support multi dbs(read/write ...)
 var sqlDb *sql.DB
+var dbMutex sync.RWMutex
 
 // one struct reflect to a table query setter
 var tableMap = map[reflect.Kind]*querySetter{}
@@ -20,8 +22,14 @@ var stmtMap = map[string]*sql.Stmt{}
 
 // Register register a database driver.
 func Register(dsn string, driver ...string) error {
-	var err error
 
+	dbMutex.Lock()
+	defer dbMutex.Unlock()
+
+	var err error
+	if sqlDb != nil {
+		return nil
+	}
 	sqlDb, err = sql.Open(append(driver, DriverMySQL)[0], dsn)
 
 	if sqlDb == nil {
