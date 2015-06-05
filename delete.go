e451578d74a7b8ee0a47f5253e0cbb0327ad4909
JessonChan <chinachendongpo@gmail.com>
2015-06-05 14:10:43 +0800

    delete 方法的更新，支持事务

--- delete.go
+++ delete.go
@@ -1,20 +1,13 @@
 package yorm
 
 import (
+	"database/sql"
 	"errors"
 	"strings"
 )
 
 func (ex *executor) Delete(clause string, args ...interface{}) (int64, error) {
-
-	if !strings.HasPrefix(strings.ToUpper(clause), "DELETE") {
-		return 0, errors.New("must be begin with delete keyword")
-	}
-	r, err := ex.exec(clause, args...)
-	if err != nil {
-		return 0, err
-	}
-	return r.RowsAffected()
+	return deleteExec(ex.exec, clause, args...)
 }
 
 //Delete delete record(s) from a table
@@ -23,11 +16,14 @@ func Delete(clause string, args ...interface{}) (int64, error) {
 }
 
 func (ex *tranExecutor) Delete(clause string, args ...interface{}) (int64, error) {
+	return deleteExec(ex.exec, clause, args...)
+}
 
+func deleteExec(exec func(clause string, args ...interface{}) (sql.Result, error), clause string, args ...interface{}) (int64, error) {
 	if !strings.HasPrefix(strings.ToUpper(clause), "DELETE") {
 		return 0, errors.New("must be begin with delete keyword")
 	}
-	r, err := ex.exec(clause, args...)
+	r, err := exec(clause, args...)
 	if err != nil {
 		return 0, err
 	}
