99541fbb8e9b90363c05281ce0bbf95bc173f954
JessonChan <chinachendongpo@gmail.com>
2020-02-10 09:16:27 +0800

    better format

--- examples/basic/basic_example.go
+++ examples/basic/basic_example.go
@@ -30,48 +30,48 @@ type ProgramLanguage struct {
 
 func main() {
 	yorm.SetLoggerLevel(yorm.Debug)
-	//设置自己的数据地址
+	// 设置自己的数据地址
 	yorm.Register("root:@tcp(127.0.0.1:3306)/yorm_test?charset=utf8")
 
-	//插入一条数据
+	// 插入一条数据
 	php := ProgramLanguage{Name: "PHP", Position: 7, RankMonth: time.Now(), Created: time.Now()}
 	yorm.Insert(&php)
 
-	//更新一条数据
+	// 更新一条数据
 	yorm.Update("update program_language set position=? where id=?", 8, php.Id)
 
-	//删除一条
+	// 删除一条
 	yorm.Delete("delete from program_language where id=? ", php.Id)
 
 	var ps []ProgramLanguage
 
-	//读取所有的数据
+	// 读取所有的数据
 	yorm.R(&ps)
 	fmt.Println(ps)
 
-	//读取所有小于10的数据
+	// 读取所有小于10的数据
 	yorm.R(&ps, "where id<10")
 	fmt.Println(ps)
 
-	//读取所有小于10的数据
+	// 读取所有小于10的数据
 	yorm.R(&ps, "where id<?", 10)
 	fmt.Println(ps)
 
-	//也可以
+	// 也可以
 	yorm.Select(&ps, "where id<?", 10)
 	fmt.Println(ps)
 
-	//也可以
+	// 也可以
 	yorm.Select(&ps, "where <10")
 	fmt.Println(ps)
 
 	var p ProgramLanguage = ProgramLanguage{Id: 1}
 
-	//读取id为1的某条数据
+	// 读取id为1的某条数据
 	yorm.R(&p)
 	fmt.Println(p)
 
-	//读取id为1的某条数据
+	// 读取id为1的某条数据
 	yorm.SelectByPK(&p)
 	fmt.Println(p)
 }
